5934ebff8eb22e65b4dc26851fd0a67c65fdc08a
Moultree <okaykudes@gmail.com>
2023-05-27 05:02:29 +0600

    added emojis

--- app/components/BoardTile/index.tsx
+++ app/components/BoardTile/index.tsx
@@ -1,17 +1,106 @@
+import EmojiPicker, { Emoji, EmojiStyle } from "emoji-picker-react";
 import Button from "../Button";
 import styles from "./style.module.css";
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useFetcher } from "@remix-run/react";
+import { useEffect, useRef, useState } from "react";
 
 const BoardTile: React.FC<BoardTileProps> = (props: BoardTileProps) => {
+    const [pickerVisibility, setVisibility] = useState<boolean>(false);
+    const pickerRef = useRef(null);
+
+    const [icon, setIcon] = useState<string>(props.icon ?? "1f4cb");
+
+    const [newBoardName, setBoardName] = useState<string>("");
+
+    function useOutsideRef(ref: React.MutableRefObject<any>) {
+        useEffect(() => {
+            function handleClickOutside(event: MouseEvent) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    setVisibility(false);
+                }
+            }
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                // Unbind the event listener on clean up
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+
+    useOutsideRef(pickerRef);
+    const fetcher = useFetcher();
+
     return (
-        <div className={`${styles.tile} ${props.asHeader ? styles.page : ""}`}>
-            <Link
-                className={styles.header}
-                to={props.asHeader ? "." : `/board/${props.id}`}
-            >
-                <div className={styles.icon}>{props.icon ?? "📋"}</div>
-                <h1>{props.editing ? <input></input> : props.name}</h1>
-            </Link>
+        <div
+            className={`${styles.tile} ${props.asHeader ? styles.page : ""} ${
+                props.editing ? styles.editing : ""
+            }`}
+        >
+            <header className={styles.header}>
+                <div
+                    className={styles.icon}
+                    onClick={() => setVisibility(true)}
+                    ref={pickerRef}
+                >
+                    <Emoji
+                        emojiStyle={EmojiStyle.TWITTER}
+                        unified={icon}
+                        size={24}
+                        lazyLoad
+                    ></Emoji>
+                    {props.asHeader ? (
+                        <div
+                            className={styles.picker}
+                            style={{
+                                display: pickerVisibility ? "block" : "none",
+                            }}
+                        >
+                            <EmojiPicker
+                                emojiStyle={EmojiStyle.TWITTER}
+                                onEmojiClick={(emoji, event) => {
+                                    setIcon(emoji.unified);
+                                    fetcher.submit(
+                                        {
+                                            emoji: emoji.unified,
+                                            _action: "setEmoji",
+                                        },
+                                        { method: "PUT", replace: true }
+                                    );
+                                }}
+                                lazyLoadEmojis
+                            />
+                        </div>
+                    ) : null}
+                </div>
+                <h1>
+                    {props.editing ? (
+                        <fetcher.Form method="POST">
+                            <input
+                                className={styles.new}
+                                type="text"
+                                name="name"
+                                placeholder="New board..."
+                                value={newBoardName}
+                                onChange={(event) =>
+                                    setBoardName(event.target.value)
+                                }
+                            ></input>
+                            <input
+                                type="submit"
+                                name="_action"
+                                value="newBoard"
+                                aria-label="newBoard"
+                                hidden
+                            />
+                        </fetcher.Form>
+                    ) : (
+                        <Link to={props.asHeader ? "." : `/board/${props.id}`}>
+                            {props.name}
+                        </Link>
+                    )}
+                </h1>
+            </header>
             {props.editing ? null : (
                 <section className={styles.buttons}>
                     <Form>
@@ -37,10 +126,10 @@ const BoardTile: React.FC<BoardTileProps> = (props: BoardTileProps) => {
 };
 
 interface BoardTileProps {
-    id: number;
+    id?: number;
     icon?: string;
-    name: string;
-    isOwner: boolean;
+    name?: string;
+    isOwner?: boolean;
     editing?: boolean;
     asHeader?: boolean;
 }
--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -1,18 +1,26 @@
+import { Emoji, EmojiStyle } from "emoji-picker-react";
 import styles from "./style.module.css";
 import type { ICard } from "~/interfaces";
 
 const Card = (props: CardProps) => {
     return (
         <div className={styles.card}>
-            <h3 className={styles.header}>Card header</h3>
-            <p className={styles.description}>Description</p>
-            <section className={styles.assignee}></section>
+            <h3 className={styles.header}>
+                <Emoji
+                    unified={props.card.emoji ?? "1f4c4"}
+                    size={24}
+                    emojiStyle={EmojiStyle.TWITTER}
+                ></Emoji>
+                {props.card.title}
+            </h3>
+            <p className={styles.description}>{props.card.description}</p>
+            <section className={styles.assignee}>{props.card.assignee}</section>
         </div>
     );
 };
 
 interface CardProps {
-    id: number;
+    card: ICard;
 }
 
 export default Card;
