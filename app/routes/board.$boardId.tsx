b7005ed08a15806a21f2fda59b6f50c4c33e439e
Moultree <okaykudes@gmail.com>
2023-05-26 20:15:59 +0600

    began creating board/{id}

--- app/routes/board.$boardId.tsx
+++ app/routes/board.$boardId.tsx
@@ -3,12 +3,49 @@ import { Link, useLoaderData } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
 import { redirect } from "react-router";
-import { Account, Navbar } from "~/components";
-import type { User, Board } from "~/interfaces";
+import { Account, BoardTile, List, Navbar } from "~/components";
+import type { IUser, IBoard } from "~/interfaces";
 import { getSession } from "~/sessions";
+import styles from "~/styles/board.module.css";
 
 export const meta: V2_MetaFunction = () => {
-    return [{ title: "Board" }, { favicon: "📋" }];
+    return [{ title: "Board" }];
+};
+
+type BoardData = {
+    user: IUser;
+    board: IBoard;
+};
+
+const loadBoard = async (cookie: string, boardId: string): Promise<IBoard> => {
+    const boardResponse = await fetch(
+        `http://localhost:8080/api/board/${boardId}`,
+        {
+            method: "GET",
+            headers: {
+                cookie: cookie,
+            },
+            credentials: "include",
+        }
+    );
+
+    if (!boardResponse.ok) {
+        console.log(boardResponse);
+    }
+
+    return boardResponse.json();
+};
+
+const loadUser = async (cookie: string): Promise<IUser> => {
+    const userResponse = await fetch("http://localhost:8080/api/account/me", {
+        method: "GET",
+        headers: {
+            cookie: cookie,
+        },
+        cache: "reload",
+    });
+
+    return userResponse.json();
 };
 
 const loader = async ({ request, params }: LoaderArgs) => {
@@ -18,25 +55,37 @@ const loader = async ({ request, params }: LoaderArgs) => {
         return redirect("/login");
     }
 
-    const boardsResponse = await fetch(
-        `http://localhost:8080/api/board/${params.boardId}`,
-        {
-            method: "GET",
-            headers: {
-                cookie: `token=${session.get("token")}`,
-            },
-            credentials: "include",
-        }
+    let board = await loadBoard(
+        `token=${session.get("token")}`,
+        params.boardId!
     );
 
-    let user = session.get("user");
-    let board = await boardsResponse.json();
+    let user =
+        session.get("user") ||
+        (await loadUser(`token=${session.get("token")}`));
 
     if (!user) {
-        const userResponse = await fetch(
-            "http://localhost:8080/api/account/me",
+        throw new Error();
+    }
+
+    return json<BoardData>({ user, board });
+};
+
+const action = async ({ request, params }: LoaderArgs) => {
+    const session = await getSession(request.headers.get("Cookie"));
+
+    if (!session.has("token")) {
+        return redirect("/login");
+    }
+
+    let formData = await request.formData();
+    let { _action } = Object.fromEntries(formData);
+
+    if (_action === "delete") {
+        const response = await fetch(
+            `http://localhost:8080/api/board/${params.boardId}`,
             {
-                method: "GET",
+                method: "DELETE",
                 headers: {
                     cookie: `token=${session.get("token")}`,
                 },
@@ -44,23 +93,14 @@ const loader = async ({ request, params }: LoaderArgs) => {
             }
         );
 
-        user = await userResponse.json();
+        if (!response.ok) {
+            throw new Error();
+        }
     }
-
-    if (!user) {
-        throw new Error();
-    }
-
-    type UserAndBoard = {
-        user: User;
-        board: Board;
-    };
-
-    return json<UserAndBoard>({ user, board });
 };
 
 const BoardPage = () => {
-    const { user, board } = useLoaderData<typeof loader>();
+    const { user, board }: BoardData = useLoaderData<typeof loader>();
 
     return (
         <main>
@@ -68,9 +108,38 @@ const BoardPage = () => {
                 <Link to="/board">My boards</Link>
                 <Account user={user} />
             </Navbar>
+            <div className={styles.board}>
+                <BoardTile
+                    id={board.id}
+                    name={board.name}
+                    isOwner={user.id == board.authorId}
+                    asHeader
+                ></BoardTile>
+                <div className={styles.lists}>
+                    <List id={1} name={"Not started"}></List>
+                    <List
+                        id={2}
+                        name={"In progress"}
+                        color="#D3E5EF"
+                        dotColor="#5B97BD"
+                    ></List>
+                    <List
+                        id={3}
+                        name={"Testing"}
+                        color="#F5E0E9"
+                        dotColor="#CD749F"
+                    ></List>
+                    <List
+                        id={4}
+                        name={"Done"}
+                        color="#DBEDDB"
+                        dotColor="#6C9B7D"
+                    ></List>
+                </div>
+            </div>
         </main>
     );
 };
 
-export { loader };
+export { loader, action };
 export default BoardPage;
