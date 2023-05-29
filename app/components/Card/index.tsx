6a7061734470eccf4dc9bf78014b3a74a878f123
Moultree <okaykudes@gmail.com>
2023-05-29 23:21:54 +0600

    added drag-n-drop functionality added ability to change card contents

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -1,25 +1,153 @@
 import { Emoji, EmojiStyle } from "emoji-picker-react";
 import styles from "./style.module.css";
-import type { ICard } from "~/interfaces";
+import type { ICard, IUser } from "~/interfaces";
+import { useDraggable } from "@dnd-kit/core";
+import { useRef, useState } from "react";
+
+import sanitizeHtml from "sanitize-html";
+import ContentEditable from "react-contenteditable";
+import { useFetcher } from "@remix-run/react";
+import Button from "../Button";
 
 const Card = (props: CardProps) => {
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+        id: props.card.id,
+        data: {
+            name: props.card.title,
+            listId: props.card.listId,
+            props: props,
+        },
+    });
+
+    const style = transform
+        ? {
+              transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+              opacity: 0.4,
+              border: "1px solid rgb(0 0 0 / 20%)",
+              zindex: 999,
+          }
+        : undefined;
+
+    const [title, _setTitle] = useState(props.card.title);
+    const titleRef = useRef(title);
+    const [description, _setDescription] = useState(props.card.description);
+    const descriptionRef = useRef(description);
+
+    const sanitizeConf = {
+        allowedTags: ["b", "i", "a", "p"],
+        allowedAttributes: { a: ["href"] },
+    };
+
+    const setTitle = (title: string) => {
+        titleRef.current = sanitizeHtml(title, sanitizeConf);
+        _setTitle(titleRef.current);
+    };
+
+    const setDescription = (description: string) => {
+        descriptionRef.current = sanitizeHtml(description, sanitizeConf);
+        _setDescription(descriptionRef.current);
+    };
+
+    const [buttonVisible, setVisible] = useState(false);
+
+    const fetcher = useFetcher();
+
     return (
-        <div className={styles.card}>
-            <h3 className={styles.header}>
+        <div
+            className={styles.card}
+            ref={setNodeRef}
+            style={style}
+            {...listeners}
+            {...attributes}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+        >
+            <div className={styles.header}>
                 <Emoji
                     unified={props.card.emoji ?? "1f4c4"}
                     size={24}
                     emojiStyle={EmojiStyle.TWITTER}
                 ></Emoji>
-                {props.card.title}
-            </h3>
-            <p className={styles.description}>{props.card.description}</p>
-            <section className={styles.assignee}>{props.card.assignee}</section>
+                <ContentEditable
+                    onChange={(event) => setTitle(event.target.value)}
+                    onBlur={(event) => {
+                        if (
+                            titleRef.current != props.card.title ||
+                            descriptionRef.current != props.card.description
+                        ) {
+                            fetcher.submit(
+                                {
+                                    cardId: props.card.id.toString(),
+                                    title: titleRef.current,
+                                    description: descriptionRef.current,
+                                    _action: "updateCardText",
+                                },
+                                { method: "PUT" }
+                            );
+                        }
+                    }}
+                    autoFocus
+                    html={title}
+                />
+            </div>
+            <ContentEditable
+                className={styles.description}
+                html={description}
+                onChange={(event) => setDescription(event.target.value)}
+                onBlur={() => {
+                    console.log(
+                        description,
+                        descriptionRef,
+                        props.card.description
+                    );
+                    if (
+                        titleRef.current != props.card.title ||
+                        descriptionRef.current != props.card.description
+                    ) {
+                        fetcher.submit(
+                            {
+                                cardId: props.card.id.toString(),
+                                title: titleRef.current,
+                                description: descriptionRef.current,
+                                _action: "updateCardText",
+                            },
+                            { method: "PUT" }
+                        );
+                    }
+                }}
+            ></ContentEditable>
+            <section className={styles.assignee}>
+                <img
+                    src={props.assignee?.avatarUrl ?? "/placeholder.png"}
+                    alt={`${props.assignee?.username}'s avatar`}
+                />
+                {props.assignee?.username}
+            </section>
+            <Button
+                mini
+                style={
+                    buttonVisible
+                        ? { display: "inline-flex" }
+                        : { display: "none" }
+                }
+                onClick={() =>
+                    fetcher.submit(
+                        {
+                            cardId: props.card.id.toString(),
+                            _action: "deleteCard",
+                        },
+                        { method: "DELETE" }
+                    )
+                }
+            >
+                <img src="/trash.svg" alt="Delete card" />
+            </Button>
         </div>
     );
 };
 
 interface CardProps {
+    assignee?: IUser | null;
     card: ICard;
 }
 
--- app/routes/board.$boardId.tsx
+++ app/routes/board.$boardId.tsx
@@ -1,7 +1,12 @@
+import type { DragEndEvent } from "@dnd-kit/core";
+import { MouseSensor, useSensor } from "@dnd-kit/core";
+import { DndContext } from "@dnd-kit/core";
 import type { V2_MetaFunction } from "@remix-run/react";
+import { useFetcher } from "@remix-run/react";
 import { Link, useLoaderData } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
+import { useState } from "react";
 import { redirect } from "react-router";
 import { Account, BoardTile, List, Navbar } from "~/components";
 import type { IUser, IBoard, IList, ICard } from "~/interfaces";
@@ -16,7 +21,25 @@ type BoardData = {
     user: IUser;
     board: IBoard;
     lists: IList[];
-    cards: ICard[][];
+    cardsWithAssignee: { assignee: IUser | null; card: ICard }[][];
+};
+
+const loadUserById = async (
+    cookie: string,
+    userId: number | string
+): Promise<IUser> => {
+    const response = await fetch(
+        `http://localhost:8080/api/account/${userId}`,
+        {
+            method: "GET",
+            headers: {
+                cookie: cookie,
+            },
+            cache: "reload",
+        }
+    );
+
+    return response.json();
 };
 
 const loadBoard = async (cookie: string, boardId: string): Promise<IBoard> => {
@@ -28,6 +51,7 @@ const loadBoard = async (cookie: string, boardId: string): Promise<IBoard> => {
                 cookie: cookie,
             },
             credentials: "include",
+            cache: "reload",
         }
     );
 
@@ -47,6 +71,7 @@ const loadLists = async (cookie: string, boardId: string): Promise<IList[]> => {
                 cookie: cookie,
             },
             credentials: "include",
+            cache: "reload",
         }
     );
 
@@ -65,6 +90,7 @@ const loadCards = async (
                 cookie: cookie,
             },
             credentials: "include",
+            cache: "no-store",
         }
     );
 
@@ -104,12 +130,33 @@ const loader = async ({ request, params }: LoaderArgs) => {
         params.boardId!
     );
 
-    let cards = [];
+    let cardsWithAssignee = [];
+    console.log(lists);
     for (const list of lists) {
-        cards.push(await loadCards(`token=${session.get("token")}`, list.id));
+        let cards = await loadCards(`token=${session.get("token")}`, list.id);
+        console.log(cards);
+        let listCards = [];
+
+        for (const card of cards) {
+            card["listId"] = list.id;
+
+            listCards.push({
+                assignee: card.assigneeId
+                    ? await loadUserById(
+                          `token=${session.get("token")}`,
+                          card.assigneeId
+                      )
+                    : null,
+                card: card,
+            });
+        }
+
+        listCards = listCards.sort((a, b) => a.card.position - b.card.position);
+
+        cardsWithAssignee.push(listCards);
     }
 
-    return json<BoardData>({ user, board, lists, cards });
+    return json<BoardData>({ user, board, lists, cardsWithAssignee });
 };
 
 const action = async ({ request, params }: LoaderArgs) => {
@@ -140,29 +187,149 @@ const action = async ({ request, params }: LoaderArgs) => {
     }
 
     if (_action === "setEmoji") {
-        const response = await fetch(
-            `http://localhost:8080/api/board/${params.boardId}`,
+        await fetch(`http://localhost:8080/api/board/${params.boardId}`, {
+            method: "PUT",
+            headers: {
+                cookie: `token=${session.get("token")}`,
+            },
+            body: JSON.stringify({
+                emoji: formData.get("emoji"),
+            }),
+            cache: "reload",
+        });
+    }
+
+    if (_action === "updateCard") {
+        await fetch(
+            `http://localhost:8080/api/card/${formData.get("cardId")}`,
             {
                 method: "PUT",
                 headers: {
                     cookie: `token=${session.get("token")}`,
                 },
+                credentials: "include",
                 body: JSON.stringify({
-                    emoji: formData.get("emoji"),
+                    listId: formData.get("listId"),
+                    position: formData.get("position"),
                 }),
                 cache: "reload",
             }
         );
+    }
 
-        console.log(response);
+    if (_action === "updateCardText") {
+        await fetch(
+            `http://localhost:8080/api/card/${formData.get("cardId")}`,
+            {
+                method: "PUT",
+                headers: {
+                    cookie: `token=${session.get("token")}`,
+                },
+                credentials: "include",
+                body: JSON.stringify({
+                    title: formData.get("title"),
+                    description: formData.get("description"),
+                }),
+                cache: "reload",
+            }
+        );
+    }
+
+    if (_action === "deleteCard") {
+        await fetch(
+            `http://localhost:8080/api/card/${formData.get("cardId")}`,
+            {
+                method: "DELETE",
+                headers: {
+                    cookie: `token=${session.get("token")}`,
+                },
+                credentials: "include",
+            }
+        );
     }
 
     return redirect(".");
 };
 
 const BoardPage = () => {
-    const { user, board, lists, cards }: BoardData =
+    const { user, board, lists, cardsWithAssignee }: BoardData =
         useLoaderData<typeof loader>();
+
+    const [cards, setCardsWithAssignee] =
+        useState<{ assignee: IUser | null; card: ICard }[][]>(
+            cardsWithAssignee
+        );
+
+    const fetcher = useFetcher();
+
+    const mouseSensor = useSensor(MouseSensor, {
+        activationConstraint: {
+            distance: 10,
+        },
+    });
+
+    function handleDragEnd(event: DragEndEvent) {
+        const { active, over } = event;
+
+        if (active.id !== over?.id) {
+            setCardsWithAssignee((items) => {
+                let itemsCopy = items;
+
+                if (!over || !active) {
+                    return items;
+                }
+
+                const oldListIndex = lists.indexOf(
+                    lists.find(
+                        (list) => list.id == active.data.current!.listId
+                    )!
+                );
+                const newListIndex = lists.indexOf(
+                    lists.find((list) => list.id == over.data.current!.listId)!
+                );
+
+                const oldIndex = itemsCopy[oldListIndex]
+                    ?.map((item) => item.card.id)
+                    ?.indexOf(active.id as number);
+
+                const newIndex = itemsCopy[oldListIndex]
+                    ?.map((item) => item.card.id)
+                    ?.indexOf(over.id as number);
+
+                if (oldIndex == -1) {
+                    return items;
+                }
+
+                const moving = itemsCopy[oldListIndex].splice(oldIndex, 1)[0];
+                moving.card.listId = over.data.current!.listId;
+
+                if (newIndex == -1) {
+                    moving.card.position = itemsCopy[newListIndex].length;
+                    itemsCopy[newListIndex].push(moving);
+                } else {
+                    moving.card.position = newIndex;
+                    itemsCopy[newListIndex].splice(newIndex, 0, moving);
+                    for (let i = 0; i < itemsCopy[newListIndex].length; i++) {
+                        itemsCopy[newListIndex][i].card.position = i;
+                    }
+                }
+
+                for (const card of cards[newListIndex]) {
+                    fetcher.submit(
+                        {
+                            cardId: card.card.id.toString(),
+                            listId: card.card.listId.toString(),
+                            position: card.card.position.toString(),
+                            _action: "updateCard",
+                        },
+                        { method: "PUT" }
+                    );
+                }
+
+                return itemsCopy;
+            });
+        }
+    }
 
     return (
         <main>
@@ -170,25 +337,27 @@ const BoardPage = () => {
                 <Link to="/board">My boards</Link>
                 <Account user={user} />
             </Navbar>
-            <div className={styles.board}>
-                <BoardTile
-                    id={board.id}
-                    name={board.name}
-                    isOwner={user.id == board.authorId}
-                    icon={board.emoji}
-                    asHeader
-                ></BoardTile>
-                <div className={styles.lists}>
-                    {lists.map((list, index) => (
-                        <List
-                            key={list.id}
-                            id={list.id}
-                            name={list.name}
-                            cards={cards[index]}
-                        ></List>
-                    ))}
+            <DndContext sensors={[mouseSensor]} onDragEnd={handleDragEnd}>
+                <div className={styles.board}>
+                    <BoardTile
+                        id={board.id}
+                        name={board.name}
+                        isOwner={user.id == board.authorId}
+                        icon={board.emoji}
+                        asHeader
+                    ></BoardTile>
+                    <div className={styles.lists}>
+                        {lists.map((list, index) => (
+                            <List
+                                key={list.id}
+                                id={list.id}
+                                name={list.name}
+                                cardsWithAssignee={cards[index]}
+                            />
+                        ))}
+                    </div>
                 </div>
-            </div>
+            </DndContext>
         </main>
     );
 };
